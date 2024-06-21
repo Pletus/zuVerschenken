b4410f1cbc480d168f88635d762335d920b5a0c7
Daniele <daniele.capa@gmail.com>
2024-06-21 12:23:06 +0200

    added first iteration of navbar

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customGray: '#f0f0f0',
+      },
+    },
   },
   plugins: [],
 }
