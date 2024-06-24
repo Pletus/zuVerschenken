5089314dee8d1eedea91ac12143b6f2b0da3e8e0
YagoPazos <yagopb@gmail.com>
2024-06-24 13:29:47 +0200

    merged succes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
-import './index.css'
-import Hero from './components/Hero'
-import React from "react";
+import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
 import Cards from "./components/Cards";
-import { useState, useEffect } from "react";;
 import Login from './components/Login';
-import "./App.css";
 import AddItem from "./components/AddItem";
 import Signup from './components/Signup';
+import "./App.css";
+import './index.css'
 
 function App() {
   const [user, setUser] = useState(false);
@@ -19,7 +19,7 @@ function App() {
 
   return (
     <>
-
+      
     </>
   )
 }
