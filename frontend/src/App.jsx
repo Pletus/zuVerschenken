b6a9a9ea63f9a52944d28d6201af86a1fdf8f4fe
YagoPazos <yagopb@gmail.com>
2024-06-25 17:37:51 +0200

    for pull

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,10 +4,10 @@ import { ToastContainer, toast } from "react-toastify";
 import Hero from "./components/Hero";
 import Login from "./components/Login";
 import AddItem from "./components/AddItem";
-import 'react-toastify/dist/ReactToastify.css';
 import Signup from "./components/Signup";
 import Layout from "./components/Layout";
 import Navbar from "./components/Navbar";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import "./index.css";
 
@@ -28,10 +28,11 @@ function App() {
           <Route path="/signup" element={<Signup />} />
           <Route
             path="/additem"
-            element={user ? <AddItem /> : <Navigate to="/login" />}
+            element={user ? <AddItem /> : <Navigate to='/login'/>}
           />
         </Route>
       </Routes>
+      <ToastContainer />
     </>
   );
 }
