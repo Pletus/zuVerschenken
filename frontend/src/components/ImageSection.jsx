68fe909819212bbc9621b999bc988bdf5f774296
hamed <raahnaward@gmail.com>
2024-07-01 15:33:49 +0200

    reveted changes

---
+++ frontend/src/components/ImageSection.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ImageCarousel from "./ImageCarousel";
+
+const ImageSection = () => {
+  return (
+    <div>
+      ImageSection<h2>Featured Items</h2>
+      <ImageCarousel />
+    </div>
+  );
+};
+
+export default ImageSection;
