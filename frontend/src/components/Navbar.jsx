d5ed64ec6ad4f984c35318ad845057d46ed0134f
Daniele <daniele.capa@gmail.com>
2024-07-22 15:48:13 +0200

    fixed color and positioning of hamburger menu for navbar on phones

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,11 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
-import { IoMenuOutline, IoCloseOutline, IoMailOpenOutline } from "react-icons/io5";
+import { IoMenuOutline, IoCloseOutline } from "react-icons/io5";
 import axios from "axios";
 import logout from "../assets/icon-logout.png";
 import login from "../assets/icon-user.png";
-import "../components/CSS/Navbar.css";
 import Logo from "../assets/logo-n.svg";
-
+import "../components/CSS/Navbar.css";
 
 const Navbar = ({ onSearch }) => {
   const [user, setUser] = useState(false);
@@ -70,8 +69,8 @@ const Navbar = ({ onSearch }) => {
   const handleSearch = () => {
     onSearch(searchItem, searchPostCode);
     navigate("/items");
-    setSearchItem('');
-    setSearchPostCode('');
+    setSearchItem("");
+    setSearchPostCode("");
   };
 
   const handleKeyDown = (e) => {
@@ -81,10 +80,12 @@ const Navbar = ({ onSearch }) => {
   };
 
   return (
-    <nav className="flex flex-col lg:flex-row justify-between md:justify-around w-full px-1 py-3  shadow-md bg-customBlue relative">
-      <div className="flex justify-between">
+    <nav className="flex flex-col lg:flex-row justify-between w-full px-1 py-3 shadow-md bg-customBlue relative">
+      <div className="flex justify-between items-center w-full lg:w-auto">
         <div className="flex items-center gap-1 ml-2 sm:gap-4 lg:pl-1">
-          <div className= " rounded-full md:mx-4 lg:mr-6 xl:mr-24"><img src={Logo} alt="" className="w-32" /></div>
+          <div className="rounded-full md:mx-4 lg:mr-6 xl:mr-24">
+            <img src={Logo} alt="Logo" className="w-32" />
+          </div>
           <div className="flex items-center space-x-1 sm:space-x-4 lg:space-x-2">
             <input
               type="text"
@@ -110,102 +111,78 @@ const Navbar = ({ onSearch }) => {
             </button>
           </div>
         </div>
-        <button onClick={handleMenuToggle} className="lg:hidden text-3xl ml-2">
-          {isMenuOpen ? (
-            <IoCloseOutline size={24} />
-          ) : (
-            <IoMenuOutline size={24} />
-          )}
+        <button onClick={handleMenuToggle} className="lg:hidden text-3xl ml-2 text-white">
+          {isMenuOpen ? <IoCloseOutline size={24} /> : <IoMenuOutline size={24} />}
         </button>
       </div>
       <ul
-        className={`flex flex-col md:flex-row lg:flex-row text-white items-end lg:items-center transition-transform transform ${
-          isMenuOpen
-            ? "max-h-full opacity-100 pt-1 pr-3 gap-2 md:gap-4 md:items-center mt-2 md:pl-40 "
-            : "max-h-0 gap-6 2xl:gap-12 opacity-0 "
-        } overflow-hidden lg:overflow-visible lg:opacity-100 lg:max-h-full w-full lg:w-auto`}
+        className={`lg:flex lg:flex-row text-white items-center lg:items-center transition-all duration-300 lg:gap-6 ${
+          isMenuOpen ? "flex flex-col items-start gap-2 mt-2 bg-customBlue p-4" : "hidden"
+        } lg:gap-6`}
       >
         <li>
-          <div>
-            <NavLink
-              to="/"
-              className={({ isActive }) =>
-                `${
-                  isActive ? "text-blue-500" : "text-white"
-                } transform transition-transform duration-200 flex md:hover:scale-125`
-              }
-            >
-              Home
-            </NavLink>
-          </div>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              `${isActive ? "text-blue-500" : "text-white"} transform transition-transform duration-200 flex md:hover:scale-125`
+            }
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/additem"
+            className={({ isActive }) =>
+              `${isActive ? "text-blue-500" : "text-white"} transform transition-transform duration-200 flex md:hover:scale-125`
+            }
+          >
+            Add Box
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/items"
+            className={({ isActive }) =>
+              `${isActive ? "text-blue-500" : "text-white"} transform transition-transform duration-200 flex md:hover:scale-125`
+            }
+          >
+            Boxes
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/wishlist"
+            className={({ isActive }) =>
+              `${isActive ? "text-blue-500" : "text-white"} transform transition-transform duration-200 flex md:hover:scale-125`
+            }
+          >
+            Wishlist
+          </NavLink>
         </li>
         <li>
           <div>
-            <NavLink
-              to="/additem"
-              className={({ isActive }) =>
-                `${
-                  isActive ? "text-blue-500" : "text-white"
-                } transform transition-transform duration-200 flex md:hover:scale-125`
-              }
-            >
-              Add Box
-            </NavLink>
-          </div>
-        </li>
-        <li>
-          <div>
-            <NavLink
-              to="/items"
-              className={({ isActive }) =>
-                `${
-                  isActive ? "text-blue-500" : "text-t-white"
-                } transform transition-transform duration-200 flex md:hover:scale-125`
-              }
-            >
-              Boxes
-            </NavLink>
-          </div>
-        </li>
-        <li>
-          <div>
-            <NavLink
-              to="/wishlist"
-              className={({ isActive }) =>
-                `${
-                  isActive ? "text-blue-500" : "text-white"
-                } transform transition-transform duration-200 flex md:hover:scale-125`
-              }
-            >
-              Wishlist
-            </NavLink>
-          </div>
-        </li>
-        <li>
-          <div className="">
             {user ? (
               <div className="flex gap-2">
                 <NavLink to="/profile" className="rounded-full overflow-hidden">
                   <img
                     src={imageUrl}
                     alt="User"
-                    className="object-cover w-10 h-10 rounded-full overflow-hidden"
+                    className="object-cover w-10 h-10 rounded-full"
                   />
                 </NavLink>
                 <button
                   onClick={handleLogout}
                   className="p-2 bg-blue-500 w-10 h-10 rounded-full hover:bg-gray-300"
                 >
-                  <img src={logout} alt="logout" className="" />
+                  <img src={logout} alt="logout" />
                 </button>
               </div>
             ) : (
               <NavLink
                 to="/login"
                 className={({ isActive }) =>
-                  `${
-                    isActive ? "text-blue-500" : "text-white"
-                  } transform transition-transform duration-200 flex hover:scale-125`
+                  `${isActive ? "text-blue-500" : "text-white"} transform transition-transform duration-200 flex hover:scale-125`
                 }
               >
                 <button className="bg-blue-500 w-auto h-8 rounded-full hover:bg-gray-300">
